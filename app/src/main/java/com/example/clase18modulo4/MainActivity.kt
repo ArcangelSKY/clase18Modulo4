d5e93810c6cd9df5f4b39807dfd8931d92a2f9ca
ArcangelSKY <angelzamoranooses@hotmail.com>
2021-04-07 21:43:18 -0400

    Initial commit ejemplo de la clase 18

---
+++ app/src/main/java/com/example/clase18modulo4/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.clase18modulo4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val activity2 = Intent(this, MainActivity2::class.java)
+            startActivity(activity2)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/clase18modulo4/MainActivity2.kt
@@ -0,0 +1,20 @@
+package com.example.clase18modulo4
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        var button2 = findViewById<Button>(R.id.button2)
+        button2.setOnClickListener {
+           /* val activity1 = Intent(this, MainActivity::class.java)
+            startActivity(activity1)*/
+            finish()
+        }
+    }
+}
